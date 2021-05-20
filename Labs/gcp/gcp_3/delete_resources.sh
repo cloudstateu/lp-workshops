d9b8327c3107bc13ddc8e90822db79f906057ef4
macborowy <noobchar12345@gmail.com>
2021-05-20 12:47:28 +0200

    labs: add script for resource remove

---
+++ Labs/gcp/gcp_3/delete_resources.sh
@@ -0,0 +1,23 @@
+# Forwarding rules
+gcloud compute forwarding-rules delete fortressnet-http-rule --global -q
+gcloud compute forwarding-rules delete fortressnet-http-ipv6-rule --global -q
+
+# Backend Service
+gcloud compute target-http-proxies delete fortressnet-http-proxy -q
+gcloud compute url-maps delete fortressnet-balancer -q
+gcloud compute backend-services delete fortressnet-backend-service --global -q
+
+# Instance Groups
+gcloud compute instance-groups managed delete europe-west1-pool --region europe-west1 -q
+gcloud compute instance-groups managed delete us-central1-pool --region us-central1 -q
+gcloud compute instance-groups managed delete asia-east1-pool --region asia-east1 -q
+
+# Instance Template
+gcloud compute instance-templates delete fort-template -q
+
+# Network
+gcloud compute firewall-rules delete fortressnet-allow-load-balancer -q
+gcloud compute networks delete fortressnet -q
+
+gcloud compute addresses delete fortressnet-ip --global -q
+gcloud compute addresses delete fortressnet-ipv6 --global -q
